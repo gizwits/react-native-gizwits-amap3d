b2795e519867309ab446649fead6fbec1915f555
王锐彬 <492960423@qq.com>
2019-05-09 15:53:25 +0800

    update

---
+++ lib/ios/AMap3D/maps/AMapMarker.h
@@ -0,0 +1,18 @@
+#import "AMapView.h"
+#import "AMapCallout.h"
+
+@interface AMapMarker : UIView
+
+@property(nonatomic, copy) RCTBubblingEventBlock onAMapPress;
+@property(nonatomic, copy) RCTBubblingEventBlock onInfoWindowPress;
+@property(nonatomic, copy) RCTBubblingEventBlock onAMapDragStart;
+@property(nonatomic, copy) RCTBubblingEventBlock onAMapDrag;
+@property(nonatomic, copy) RCTBubblingEventBlock onAMapDragEnd;
+
+- (MAAnnotationView *)annotationView;
+- (MAPointAnnotation *)annotation;
+- (void)setActive:(BOOL)active;
+- (void)setMapView:(AMapView *)mapView;
+- (void)lockToScreen:(int)x y:(int)y;
+
+@end
